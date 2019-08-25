64e98b0f0cb67a69c271ce70374e7ffb43a9cf01
Andrew Santos <asantos@alumni.nd.edu>
2019-08-25 18:43:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (names, event){
+    const messageArray = [];
+    for (var i = 0; i < names.length; i++){
+        messageArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messageArray;
+}
+
+function countdown (num){
+    while (num >= 0){
+        console.log(num);
+        num --;
+    }
+}
